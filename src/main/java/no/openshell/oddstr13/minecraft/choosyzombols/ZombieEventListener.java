ca789b4a3648f154dcdfd66552b414714b3e0c7b
Odd Stråbø <oddstr13@openshell.no>
2022-01-11 06:41:57 +0100

    Match against namespaced item id, not name

--- src/main/java/no/openshell/oddstr13/minecraft/choosyzombols/ZombieEventListener.java
+++ src/main/java/no/openshell/oddstr13/minecraft/choosyzombols/ZombieEventListener.java
@@ -19,7 +19,7 @@ public class ZombieEventListener implements Listener {
 	public void onZombiePickup(EntityPickupItemEvent event) {
 		if (plugin.getConfigEnabled()) {
 			Entity entity = event.getEntity();
-			String item_name = event.getItem().getName();
+			String item_name = event.getItem().getItemStack().getType().getKey().toString();
 			if (plugin.getConfigDebug()) {
 				plugin.getLogger().log(Level.INFO, "Item {0} picked up by {1} @ {2}",
 						new Object[] { item_name, entity.getName(), entity.getLocation() });
