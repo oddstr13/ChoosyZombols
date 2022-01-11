1c3e3a577d5edf256d0d8cb54907545d275392a8
Odd Stråbø <oddstr13@openshell.no>
2022-01-11 06:59:50 +0100

    Remove extra debug log

--- src/main/java/no/openshell/oddstr13/minecraft/choosyzombols/ZombieEventListener.java
+++ src/main/java/no/openshell/oddstr13/minecraft/choosyzombols/ZombieEventListener.java
@@ -19,12 +19,8 @@ public class ZombieEventListener implements Listener {
 	public void onZombiePickup(EntityPickupItemEvent event) {
 		if (plugin.getConfigEnabled()) {
 			Entity entity = event.getEntity();
-			String item_name = event.getItem().getItemStack().getType().getKey().toString();
-			if (plugin.getConfigDebug()) {
-				plugin.getLogger().log(Level.INFO, "Item {0} picked up by {1} @ {2}",
-						new Object[] { item_name, entity.getName(), entity.getLocation() });
-			}
 			if (entity instanceof Zombie) {
+				String item_name = event.getItem().getItemStack().getType().getKey().toString();
 				boolean in_list = plugin.getConfigList().contains(item_name);
 				boolean reject = plugin.getConfigReject();
 
